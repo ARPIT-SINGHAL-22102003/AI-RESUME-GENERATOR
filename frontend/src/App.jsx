5377af8cb77b051046d9a35e2bb67691ecc3b81d
ARPIT SINGHAL <161847701+ARPIT-SINGHAL-22102003@users.noreply.github.com>
2025-04-05 10:15:13 +0000

    initiated routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,14 @@
 import { useState } from 'react'
 import { Button } from './components/ui/button'
 import './App.css'
+import { Outlet } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className="flex flex-col items-center justify-center min-h-svh">
-      <Button>Click me</Button>
-    </div>
+      <Outlet />
     </>
   )
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,32 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, Router, RouterProvider } from 'react-router-dom'
+import SigninPage from './auth/sign-in'
+import { Home } from 'lucide-react'
 
+
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children:[
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: '/dashboard',
+        element: <div>Dashboard</div>
+      }
+    ]
+  },
+  {
+    path: '/auth/signin',
+    element:<SigninPage />,
+  }
+])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
